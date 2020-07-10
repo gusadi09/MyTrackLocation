4dfc1125e3b630955f7a2379847222f38a6812a3
gusadi <gusadi872@gmail.com>
2020-07-10 13:32:39 +0800

    Adding Location Track Function

--- MyTrackLocation/ViewController.swift
+++ MyTrackLocation/ViewController.swift
@@ -7,14 +7,63 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    private var locations: [MKPointAnnotation] = []
+    
+    private lazy var locationManager: CLLocationManager = {
+        let manager = CLLocationManager()
+        
+        manager.delegate = self
+        manager.requestAlwaysAuthorization()
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.allowsBackgroundLocationUpdates = true
+        
+        return manager
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func switchPressed(_ sender: UISwitch) {
+        if sender.isOn {
+            locationManager.startUpdatingLocation()
+        } else {
+            locationManager.stopUpdatingLocation()
+        }
+    }
+    
+}
 
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let mostRecentLocation = locations.last else {return}
+        
+        let annotationToAdd = MKPointAnnotation()
+        annotationToAdd.coordinate = mostRecentLocation.coordinate
+        
+        self.locations.append(annotationToAdd)
+        
+        while self.locations.count > 50 {
+            if let annotationToRemove = self.locations.first {
+                self.locations.remove(at: 0)
+                mapView.removeAnnotation(annotationToRemove)
+            }
+        }
+        
+        if UIApplication.shared.applicationState == .active {
+            mapView.showAnnotations(self.locations, animated: true)
+        } else {
+            print("Aplikasi dalam background state. Lokasi baru saat ini ", mostRecentLocation)
+        }
+    }
 }
 
