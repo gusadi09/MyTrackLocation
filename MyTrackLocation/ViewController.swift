06650571727b1e47e4af8f64fa3331d145de0a77
gusadi <gusadi872@gmail.com>
2020-07-10 12:50:30 +0800

    Initial Commit

---
+++ MyTrackLocation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyTrackLocation
+//
+//  Created by Agus Adi Pranata on 10/07/20.
+//  Copyright © 2020 gusadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
